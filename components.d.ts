5a3985919b8d00985995e6317e3d893959d64eed
cheer <2224725460@qq.com>
2022-02-15 21:15:38 +0800

    update docs

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ declare module 'vue' {
     PageContainer: typeof import('./src/components/PageContainer.vue')['default']
     PageHeader: typeof import('./src/components/PageHeader.vue')['default']
     VanCell: typeof import('vant/es')['Cell']
-    VanDivider: typeof import('vant/es')['Divider']
     VanField: typeof import('vant/es')['Field']
     VanIcon: typeof import('vant/es')['Icon']
   }
