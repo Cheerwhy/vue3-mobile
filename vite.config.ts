aad458d252a38fa2912484df9eb3675d84c8a47b
cheer <2224725460@qq.com>
2022-02-15 13:40:58 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import vue from "@vitejs/plugin-vue";
-import viteCompression from "vite-plugin-compression";
-import Components from "unplugin-vue-components/vite";
-import { VantResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import viteCompression from 'vite-plugin-compression';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue3-mobile",
+  base: '/vue3-mobile',
   server: {
     port: 3000,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     open: true,
     https: false,
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      '@': resolve(__dirname, 'src'),
     },
   },
   plugins: [
@@ -26,11 +26,12 @@ export default defineConfig({
       verbose: true,
       disable: false,
       threshold: 10240,
-      algorithm: "gzip",
-      ext: ".gz",
+      algorithm: 'gzip',
+      ext: '.gz',
     }),
   ],
   build: {
+    outDir: 'vue3-mobile',
     terserOptions: {
       compress: {
         drop_console: true,
