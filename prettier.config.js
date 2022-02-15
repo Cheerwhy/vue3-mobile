a1926c2c1b7535ac8758036da41ca21633823698
cheer <2224725460@qq.com>
2022-02-15 13:33:08 +0800

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true, // 使用单引号
+  tabWidth: 2, // 一个 tab 键缩进相当于 2 个空格
+  useTabs: false, // 行缩进使用 tab 键代替空格
+  semi: true, // 语句的末尾加上分号
+  printWidth: 120, // 单行代码超出 120 个字符自动换行
+};
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "unplugin-vue-components/resolvers";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    open: true,
+    https: false,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    vue(),
+    Components({ resolvers: [VantResolver()] }),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+});
